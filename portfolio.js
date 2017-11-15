857731cf135171b88bcf6c565062822fbe0b8c10
8rdownes <31481017+8rdownes@users.noreply.github.com>
2017-11-15 10:07:47 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,27 @@
+function Portfolio(){
+    //instance variable
+    this.stocks=[];
+
+
+    //helper function
+    function findStock(trk,list){
+        //i represents the index of the trk you are looking for in this.stocks
+        let i;
+        for(s=0;s<this.stocks.lengths;s++;){
+            if(tkr==this.stock[s].ticker{
+              return s;
+
+            }
+        }
+        return i;
+    }
+
+    //instance functions
+    this.add = function(stk){};
+    this.update = function(tkr,val){};
+    this.buy = function(tkr,qnt){};
+    this.sell = function(tkr,qnt){};
+    this.totalValue = function(){};
+}
+
+module.exports = Portfolio;
