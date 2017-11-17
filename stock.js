debd12261498f5a64c5e1f991a03607429c09cd5
8rdownes <31481017+8rdownes@users.noreply.github.com>
2017-11-17 09:15:25 -0600

    Add files via upload

--- stock.js
+++ stock.js
@@ -1,13 +1,38 @@
-function Stock(/*fill*/){
-
-    //Initialize the instance variables below
+function Stock(n,t,i,v,q){
 
 
-    //declare and define totalValue function below
+
+this.name=n;
+this.ticker=t;
+this.index=i;
+this.value=v;
+this.quantity=q;
+
+
+//Initialize the instance variables below
+
+
+
+
+
+//declare and define totalValue function below
 
 
     //declarea and define sell(q) function below
     
+
+this.totalValue()=function(){
+return(this.value*this.quantity);
+
+this.sell(q)=function(){
+
+
+
+
+
+
 }
+
+
 
 module.exports = Stock;
